4b705dae4d54f94576b3b412848cb1225f13f929
yusukemorita <yusuke.ppq@gmail.com>
2024-01-23 16:41:54 +0100

    add SignatureAlgorithm and SignatureDevice struct types

--- signing-service-challenge-go/domain/device.go
+++ signing-service-challenge-go/domain/device.go
@@ -1,3 +1,20 @@
 package domain
 
-// TODO: signature device domain model ...
+type SignatureAlgorithm string
+
+const (
+	RSS SignatureAlgorithm = "RSS"
+	ECC SignatureAlgorithm = "ECC"
+)
+
+type SignatureDevice struct {
+	uuid              string
+	algorithm         SignatureAlgorithm
+	encodedPrivateKey []byte
+	// (optional) user provided string to be displayed in the UI
+	label string
+	// track the last signature created with this device
+	lastSignature string
+	// track how many signatures have been created with this device
+	signatureCounter uint
+}
