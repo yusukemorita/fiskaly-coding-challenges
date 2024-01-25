6b6f77d2f98105ccd4d61002113de2a10df45758
yusukemorita <yusuke.ppq@gmail.com>
2024-01-25 13:45:19 +0100

    implement minimal SignatureService that returns blank response

--- signing-service-challenge-go/api/device.go
+++ signing-service-challenge-go/api/device.go
@@ -1,3 +1,26 @@
 package api
 
+import (
+	"net/http"
+
+	"github.com/fiskaly/coding-challenges/signing-service-challenge/domain"
+)
+
+type SignatureService struct {
+	signatureDeviceRepository domain.SignatureDeviceRepository
+}
+
+func NewSignatureService(signatureDeviceRepository domain.SignatureDeviceRepository) SignatureService {
+	return SignatureService{
+		signatureDeviceRepository: signatureDeviceRepository,
+	}
+}
+
 // TODO: REST endpoints ...
+type CreateSignatureDeviceResponse struct {
+	signatureDeviceId string
+}
+
+func (s *SignatureService) CreateSignatureDevice(response http.ResponseWriter, request *http.Request) {
+	WriteAPIResponse(response, http.StatusOK, "")
+}
